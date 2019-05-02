221211fb0c5495026fa62edd4c4d03d315d0a89e
Steve Yen <steve.yen@gmail.com>
2019-05-01 19:48:56 -0700

    added package comment

--- rhmap.go
+++ rhmap.go
@@ -9,6 +9,8 @@
 //  express or implied. See the License for the specific language
 //  governing permissions and limitations under the License.
 
+// Package rhmap provides a robin-hood implementation of a hashmap for
+// golang. In other words, map[[]byte][]byte.
 package rhmap
 
 import (
